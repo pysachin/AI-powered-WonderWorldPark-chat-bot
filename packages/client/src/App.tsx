7a122a4b0fe9f7f6c3dfbd58d81358d4d3ab7026
Sachin <MS_SachinKumar_GO@outlook.com>
2025-10-02 12:17:42 +0530

    setup shadcn UI

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message,setMessage] = useState('')
@@ -15,6 +16,9 @@ function App() {
   return (
     <div className="App">
       <h1 className="font-bold p-4">{message}</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      </div>
     </div>
   )
 }
