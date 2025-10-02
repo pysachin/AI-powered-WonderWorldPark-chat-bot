ccc93e10c11ab81ecb3bbc2a1cef203935767dcf
Sachin <MS_SachinKumar_GO@outlook.com>
2025-10-02 12:30:13 +0530

    setup prettier

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   );
 } else {
   throw new Error("Root element with id 'root' not found");
